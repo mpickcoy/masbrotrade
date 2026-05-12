48d9cbcab823570dbbe09526677bedf2cf0dbc06
mpickcoy <mpickcoy@gmail.com>
2026-05-13 03:25:19 +0700

    Delete src/lib/format.ts

--- src/lib/format.ts
+++
@@ -1,5 +0,0 @@
-export const fmtMoney = (n: number, currency = "USD") =>
-  new Intl.NumberFormat("en-US", { style: "currency", currency, maximumFractionDigits: 2 }).format(n || 0);
-
-export const fmtNum = (n: number) =>
-  new Intl.NumberFormat("en-US", { maximumFractionDigits: 2 }).format(n || 0);
